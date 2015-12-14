85f9807ead3f50fac3154f4b5d79ea2cc3f3b746
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-12-13 20:11:16 -0500

    Refactor function names to be in snake case

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -1,46 +1,46 @@
 defmodule Day1 do
-  def getFloor do
-    calculateFloor([])
+  def get_floor do
+    calculate_floor([])
   end
 
-  def getFloor(instructions) do
+  def get_floor(instructions) do
     instructions
     |> String.codepoints
-    |> calculateFloor
+    |> calculate_floor
   end
 
-  defp calculateFloor(_, count \\ 0)
-  defp calculateFloor([], count) do
+  defp calculate_floor(_, count \\ 0)
+  defp calculate_floor([], count) do
     count
   end
 
-  defp calculateFloor([head|tail], count) do
+  defp calculate_floor([head|tail], count) do
     case head do 
-      "(" -> calculateFloor(tail,count + 1)
-      ")" -> calculateFloor(tail,count - 1)
-      _   -> calculateFloor(tail,count)
+      "(" -> calculate_floor(tail,count + 1)
+      ")" -> calculate_floor(tail,count - 1)
+      _   -> calculate_floor(tail,count)
     end
   end
 
-  def stopAtFloor(instructions, stopAt) do 
+  def stop_at_floor(instructions, stopAt) do 
     instructions
     |> String.codepoints
-    |> findFloor stopAt
+    |> find_floor stopAt
   end
 
-  def findFloor(_, _, floor \\ 0, count \\ 0)
-  def findFloor([], stopAt, floor, count) do
+  def find_floor(_, _, floor \\ 0, count \\ 0)
+  def find_floor([], stopAt, floor, count) do
     "Arrived at floor #{floor} in #{count} steps"
   end
-  def findFloor(_, stopAt, floor, count) when stopAt == floor do 
+  def find_floor(_, stopAt, floor, count) when stopAt == floor do 
     "Arrived at floor #{floor} in #{count} steps"
   end
 
-  def findFloor([head|tail], stopAt, floor, count) do 
+  def find_floor([head|tail], stopAt, floor, count) do 
    case head do 
-     "(" -> findFloor(tail, stopAt, floor + 1, count + 1)
-     ")" -> findFloor(tail, stopAt, floor - 1, count + 1)
-     _   -> findFloor(tail, stopAt, floor, count)
+     "(" -> find_floor(tail, stopAt, floor + 1, count + 1)
+     ")" -> find_floor(tail, stopAt, floor - 1, count + 1)
+     _   -> find_floor(tail, stopAt, floor, count)
    end
   end
 
--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -7,26 +7,26 @@ defmodule Day1Test do
   end
 
   test "the floor should be zero to start" do 
-    assert Day1.getFloor() == 0
+    assert Day1.get_floor() == 0
   end
 
   test "the elevator should go up" do
     instructions = "("
-    assert Day1.getFloor(instructions) == 1
+    assert Day1.get_floor(instructions) == 1
   end
 
   test "the elevator should go down" do
     instructions = ")"
-    assert Day1.getFloor(instructions) == -1
+    assert Day1.get_floor(instructions) == -1
   end
 
   test "the elevator can go up and down multiple floors" do
     instructions = "((()"
-    assert Day1.getFloor(instructions) == 2
+    assert Day1.get_floor(instructions) == 2
   end
 
   test "the elevator can stop at the basement" do
     instructions = "((())))"
-    assert Day1.stopAtFloor(instructions, -1) == "Arrived at floor -1 in 7 steps"
+    assert Day1.stop_at_floor(instructions, -1) == "Arrived at floor -1 in 7 steps"
   end
 end
